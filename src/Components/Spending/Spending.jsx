fde6decfb41f06151d5fd72ddc4dcf59b2bff3cb
rincaveski <jrincaveski@gmail.com>
2023-03-01 22:54:03 -0300

    design e imagens

--- src/Components/Spending/Spending.jsx
+++ src/Components/Spending/Spending.jsx
@@ -4,8 +4,8 @@ import styles from "./Spending.module.css";
 export const Spending = () => {
   return (
     <>
-      <div>
-        <p>Spending - Last 7 Days</p>
+      <div className={styles.titulo}>
+        <h1>Spending - Last 7 Days</h1>
       </div>
       <div>
         <canvas>
@@ -13,10 +13,14 @@ export const Spending = () => {
         </canvas>
       </div>
       <hr />
-      <div>
-        <div>
+      <div className={styles.rodape}>
+        <div className={styles.mesAtual}>
             <p>Total this month</p>
-            
+            <h2>$478.33</h2>
+        </div>
+        <div className={styles.mesPassado}>
+            <p>+2.4%</p>
+            <p>from last month</p>
         </div>
       </div>
     </>
