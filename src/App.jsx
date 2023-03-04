7520b0dd498097d97328db7e6a3da3d723450605
maicondguerian <maicondguerian@gmail.com>
2023-03-03 21:09:24 -0300

    chartJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import "./resetCSS.css";
 import { Spending } from "./Components/Spending/Spending";
 import { Balance } from "./Components/Balance/Balance";
+import Chart from "./Components/Chart/Chart";
 
 function App() {
   return (
     <>
-      {/* <Spending /> */}
-      <Balance />
+      <main className="App">
+        {/* <Spending /> */}
+        <Balance />
+        <Chart />
+      </main>
     </>
-    
-  )
+  );
 }
 
 export default App;
